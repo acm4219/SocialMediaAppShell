f88cbcc9caafc3a1b28878cd4fed4233087a0b13
Alex McDonald <acm4219@gmail.com>
2021-02-01 13:40:21 -0600

    got the menu set as well as the links between the pages

--- socialmediatemplate/client/src/App.js
+++ socialmediatemplate/client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+import Navbar from "./components/Navbar"
+import Home from "./views/Home";
+import Login from "./views/Login"
+import Register from "./views/Register"
+
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
