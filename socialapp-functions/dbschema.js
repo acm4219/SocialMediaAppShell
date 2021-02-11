62156f8082d00137d7328d766b4cd8183ed125ed
Alex McDonald <acm4219@gmail.com>
2021-02-10 23:04:57 -0600

    updated the engine in the package json and updated the routes to utilize express

---
+++ socialapp-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'this is the post body',
+            createdAt: '2021-02-11T05:01:29.771Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -3,35 +3,32 @@ const admin = require('firebase-admin')
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
-});
+const express = require('express');
+const app = express();
 
-exports.getPosts = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('posts').get()
+app.get('/posts', (req, res) => {
+    admin.firestore().collection('posts').orderBy('createdAt', 'desc').get()
     .then(data => {
         let posts = [];
         data.forEach(doc => {
-            posts.push(doc.data());
+            posts.push({
+                postId: doc.id,
+                body: doc.data().vody,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            });
         });
 
         return res.json(posts);
     })
     .catch(err => console.error(err));
-});
+})
 
-exports.createPost = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST'){
-        return res.status(400).json({error: 'Method not allowed'})
-    }
+app.post('/post', (req, res) => {
     const newPost = {
         body: req.body.body, 
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     }
 
     admin
@@ -46,3 +43,7 @@ exports.createPost = functions.https.onRequest((req, res) => {
          console.error(err);
      });
 });
+
+// https://baseurl.com/api/screams
+
+exports.api = functions.https.onRequest(app);
